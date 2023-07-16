8d3596f7dacf2dc16f013ca65e0ee01a5abd420e
cat pouch <pouchofcats@gmail.com>
2023-07-15 20:01:34 -0700

    cleaned up benchmarks

--- benchmarks/hll_bench.py
+++ benchmarks/hll_bench.py
@@ -6,37 +6,12 @@ f = open("benchmarks/hll_data", "rb")
 
 data = pickle.load(f)
 
-hll1 = HyperLogLog(0.00408)
-hll2 = HyperLogLog(0.00408)
+hll = HyperLogLog(0.00408)
 
-hll_union = HyperLogLog(0.00408)
-hll_intersect = HyperLogLog(0.00408)
+for x in data:
+    hll.push(x)
 
-for x in data[:len(data)//2]:
-    hll1.push(x)
-    hll_union.push(x)
-    hll_intersect.push(x)
-
-for x in data[len(data)//2:]:
-    hll2.push(x)
-
-print(f"1 cardinality: {hll1.len()}")
-print(f"2 cardinality: {hll2.len()}")
-
-hll_union.union(hll2)
-
-print(f"union cardinality: {hll_union.len()}")
-
-hll_intersect.intersect(hll2)
-
-print(f"intersect cardinality: {hll_intersect.len()}")
-
-print(f"empty?: {hll1.is_empty()}")
-
-hll1.clear()
-
-print(f"cleared cardinality: {hll1.len()}")
-print(f"empty?: {hll1.is_empty()}")
+print(hll.len())
 
 # data = []
 
--- benchmarks/topk_bench.py
+++ benchmarks/topk_bench.py
@@ -3,17 +3,9 @@ from random import choice
 
 topk = TopK(20, .99, .002)
 
-print(f"capacity: {topk.capacity()}")
-
 keys = ["{:02x}".format(x) for x in range(256)]
 
 for _ in range(1000000):
     topk.push(choice(keys), 1)
 
 print(topk.top())
-
-topk.clear()
-
-print(f"cleared: {topk.top()}")
-
-# print(keys)
